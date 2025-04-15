093cee7376faaa4bfc398a388b9692af5a30c154
Aidan5775 <alpotter0704@gmail.com>
2025-04-14 22:06:52 -0500

    Updated nomenclature for client and server.

--- client.py
+++ client.py
@@ -7,32 +7,32 @@ import threading
 nickname = input("Enter a nickname to be displayed: ")
 
 # Create a socket and connect to the server
-csclient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-csclient.connect(('127.0.0.1', 55555))  # IP must match the server's host and port
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))  # IP must match the server's host and port
 
 # receive function will handle incoming messages 
 def receive():
     while True:
         try:
-            msg = csclient.receive(1024).decode('utf-8')
+            message = client.receive(1024).decode('utf-8')
             # If the server requests a nickname, send it
-            if msg == 'NICK':
-                csclient.send(nickname.encode('utf-8'))
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
             else:
                 # Print all other messages from the server
-                print(msg)
+                print(message)
         except:
             # In case of an error, like a shutdown, leave the server
             print("An error has occurred! Disconnecting you from the server.")
-            csclient.close()
+            client.close()
             break
 
 # write function will handle sending messages
 def write():
     while True:
         # Read an input from the user and send it
-        msg = f'{nickname}: {input("")}'
-        csclient.send(msg.encode('utf-8'))
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('utf-8'))
 
 # Start receiving and writing messages in separate threads so they can run simultaneously
 receive_thread = threading.Thread(target=receive)
--- server.py
+++ server.py
@@ -4,9 +4,9 @@ import threading
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 55555
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((SERVER_HOST, SERVER_PORT))
-server.listen()
+srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv.bind((SERVER_HOST, SERVER_PORT))
+srv.listen()
 print(f'Server started on {SERVER_HOST}:{SERVER_PORT}')
 
 client_list = []
@@ -16,16 +16,16 @@ def broadcast(message):
     for client in client_list:
         client.send(message)
 
-def handle(csclient):
+def handle(client):
     while True:
         try:
-            message = input.receive(1024)     
+            message = input.recv(1024)     
             broadcast(message)          
 
         except:
-            index = client_list.index(csclient)
-            client_list.remove(csclient)
-            csclient.close()
+            index = client_list.index(client)
+            client_list.remove(client)
+            client.close()
             nickname = nickname_list[index]
             broadcast(f'{nickname} has left the chat!'.encode('utf-8'))
             nickname_list.remove(nickname)
@@ -33,21 +33,22 @@ def handle(csclient):
 
 def receive():
     while True:
-        crclient, address = server.accept()
-        print(f'Connected with {str(address)}')
+        client, address = srv.accept()
+        print(f'Connected on {str(address)}')
 
-        crclient.send('NICK'.encode('utf-8'))
-        nickname = crclient.receive(1024).decode('utf-8')
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+
         nickname_list.append(nickname)
-        client_list.append(crclient)
+        client_list.append(client)
 
         print(f'The Nickname of the Client is {nickname}!')
 
         broadcast(f'{nickname} has joined the chat!'.encode('utf-8'))
 
-        crclient.send('Connected to the server!'.encode('utf-8'))
+        client.send('Connected to the server!'.encode('utf-8'))
 
-        thread = threading.Thread(target=handle, args=(crclient,))
+        thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
 receive()
